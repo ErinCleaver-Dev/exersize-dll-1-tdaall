af92acdeefcf5e14be7daa8bd303224718bfb1fb
ecleaver <37559968+ecleaver@users.noreply.github.com>
2021-06-06 15:00:55 -0400

    Pushing to stack blitzs

--- index.js
+++ index.js
@@ -14,9 +14,54 @@ class DoubleLinkedList {
     this.length = 0;
   }
 
-  push(value) {}
-  pop() {}
-  shift() {}
+  push(value) {
+    const newNode = new Node(value);
+    
+    // checks to see if the node exists 
+    if(!this.length) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail; 
+      this.tail = newNode;
+    }
+    this.length ++;
+  }
+  pop() {
+    let removeNode = this.tail;
+
+    if(!this.length) { 
+      return null;
+    } else {
+      // to hold the item to be removed 
+      if(this.length == 1) {
+        this.head = null;
+        this.tail = null;
+      } else {
+        this.tail = this.tail.prev;
+        // removes the old connections
+        this.tail.next = null
+        removeNode.prev = null;
+      }
+    }
+
+  }
+  shift() {
+    let removeNode = this.head;
+
+    if(!this.length) { 
+      return null;
+    } else {
+      // to hold the item to be removed 
+      this.head = removeNode.next;
+
+      // removes the old connections
+      this.head.prev = null
+      removeNode.next = null;
+    }
+    this.length --;
+  }
 }
 
 // Tests
